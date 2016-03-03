29387777b95b7d67391ed20151e568d0079dd4d4
Yordan Yordanov <2038011y@student.gla.ac.uk>
2016-03-03 13:35:00 +0000

    Modified log messages for easier transfer to csv files.

--- public/js/global.js
+++ public/js/global.js
@@ -22,3 +22,28 @@ var options = {
 };
 var firstQuery = '#query_First';
 var secondQuery = '#query_Second';
+
+// calculating user clicks and time taken
+var number = 0;
+var lastClick = 0;
+var startTimer;
+var startTime = 0;
+
+console.log('click,timeperclick,totaltime');
+document.onclick = function () {
+    number++;
+    if (startTimer === undefined) {
+        startTimer = new Date();
+        startTime = startTimer.getTime();
+    }
+    var d = new Date();
+    var t = d.getTime();
+    if (lastClick === 0) {
+        console.log(number + ',' + 0 + ',' + (t - startTime) / 1000);
+    }
+    else {
+        console.log(number + ',' + (t - lastClick) / 1000 + ',' + (t - startTime) / 1000);
+    }
+    lastClick = t;
+
+};
